c48e37a2aedfca014e325a9c99923c9a8b7b0c71
Aya-Ramadan <ayaramadan@gmail.com>
2024-11-22 17:24:04 +0200

    Bit Math File

---
+++ COTS/1-LIB/STD_TYPES.h
@@ -0,0 +1,30 @@
+/*
+ *################################  STD_TYPES.h  ######################
+ *   Author : Aya Ramadan
+ *   Date : 29 November 2023
+ *   Layer :LIB
+*/
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+typedef char                     s8;
+typedef short int                s16;   
+typedef long int                 s32;      
+typedef long long int            s64;
+typedef float                    f32;
+typedef double                   f64;
+typedef long double              f128;
+typedef unsigned char            u8;
+typedef unsigned short int       u16;
+typedef unsigned long int        u32;
+typedef unsigned long long int   u64;
+
+#define STD_TYPES_OK    1
+#define STD_TYPES_NOK   0
+
+#define   NULL   ((void*)0)
+
+#endif
+ 
+
