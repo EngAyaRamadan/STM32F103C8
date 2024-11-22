e01eab777b5e0c4d941835f2828dd833a83e0433
Aya-Ramadan <ayaramadan@gmail.com>
2024-11-22 17:51:47 +0200

    GPIO Driver

---
+++ COTS/2-MCAL/02-GPIO/GPIO_config.h
@@ -0,0 +1,22 @@
+/*
+ *################################  GPIO_config.h  ######################
+ *   Author : Aya Ramadan
+ * 	 Date   : 24 October 2024
+ *   Version: 1
+ *   SWC    : GPIO
+*/
+
+#ifndef GPIO_CONFIG_H
+#define GPIO_CONFIG_H
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ COTS/2-MCAL/02-GPIO/GPIO_private.h
@@ -0,0 +1,22 @@
+/*
+ *################################  GPIO_private.h  ######################
+ *   Author : Aya Ramadan
+ * 	 Date   : 24 October 2024
+ *   Version: 1
+ *   SWC    : GPIO
+*/
+
+#ifndef GPIO_PRIVATE_H
+#define GPIO_PRIVATE_H
+
+
+
+
+
+
+
+
+
+
+
+#endif
